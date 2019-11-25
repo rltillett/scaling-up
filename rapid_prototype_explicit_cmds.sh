1454ffa28883948e57483b1e8b0c29c6acec4c0a
Richard Tillett <rltillett@users.noreply.github.com>
2019-11-25 13:58:54 -0800

    typos

--- rapid_prototype_explicit_cmds.sh
+++ rapid_prototype_explicit_cmds.sh
@@ -7,5 +7,5 @@ trimmomatic PE SRR2589044_1.fastq.gz SRR2589044_2.fastq.gz \
              SLIDINGWINDOW:4:20 MINLEN:25 ILLUMINACLIP:NexteraPE-PE.fa:2:40:15
 # bwa command here
 bwa mem -t 2 \
-/ref_genome/ecoli_rel606.fasta \
+../ref_genome/ecoli_rel606.fasta \
 SRR2589044_1.trim.fastq.gz SRR2589044_2.trim.fastq.gz > SRR2589044.sam
